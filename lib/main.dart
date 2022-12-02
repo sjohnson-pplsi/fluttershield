bd98fd56a54bfea6f5a758df9b9321e8bb11312e
Sean Johnson <seanjohnson@pplsi.com>
2022-12-02 10:31:28 -0700

    Created AppBar.

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,6 @@ import 'package:fluttershield/call_firm_screen.dart';
 import 'package:fluttershield/home_screen.dart';
 import 'package:fluttershield/requests_screen.dart';
 
-import 'login_screen.dart';
-import 'stopwatch.dart';
-
 void main() => runApp(const FlutterShield());
 
 class FlutterShield extends StatelessWidget {
@@ -13,7 +10,13 @@ class FlutterShield extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: RootScreen());
+    return MaterialApp(
+        home: const RootScreen(),
+        theme: ThemeData(
+            appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 1,
+        )));
   }
 }
 
@@ -24,8 +27,22 @@ class RootScreen extends StatefulWidget {
   State<RootScreen> createState() => _RootScreenState();
 }
 
+class Destination {
+  final String label;
+  final Icon icon;
+
+  const Destination({required this.icon, required this.label});
+}
+
 class _RootScreenState extends State<RootScreen> {
   int index = 0;
+  final List<Destination> destinations = const [
+    Destination(icon: Icon(Icons.home), label: "Home"),
+    Destination(icon: Icon(Icons.folder), label: "Requests"),
+    Destination(icon: Icon(Icons.call), label: "Call Firm"),
+    Destination(icon: Icon(Icons.inbox), label: "Inbox"),
+    Destination(icon: Icon(Icons.account_circle), label: "Profile"),
+  ];
 
   _selectScreen(int nextIndex) {
     setState(() {
@@ -35,19 +52,60 @@ class _RootScreenState extends State<RootScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-          selectedIndex: index,
-          onDestinationSelected: _selectScreen,
-          destinations: const [
-            NavigationDestination(icon: Icon(Icons.home), label: ""),
-            NavigationDestination(icon: Icon(Icons.folder), label: ""),
-            NavigationDestination(icon: Icon(Icons.call), label: ""),
-            NavigationDestination(icon: Icon(Icons.inbox), label: ""),
-            NavigationDestination(icon: Icon(Icons.account_circle), label: ""),
+    return LayoutBuilder(builder: (context, dimens) {
+      if (dimens.maxWidth >= 600) {
+        return Scaffold(
+          appBar: AppBar(
+              title: Row(
+            children: [
+              Image.asset("assets/logo.png"),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text("Legal",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black)),
+              ),
+              Expanded(child: Container()),
+              IconButton(
+                  onPressed: null,
+                  icon:
+                      Icon(Icons.help_outline_outlined, color: Colors.black54)),
+              IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.person_outline,
+                    color: Colors.black54,
+                  )),
+              Text("User", style: TextStyle(color: Colors.black54))
+            ],
+          )),
+          body: Row(children: [
+            NavigationRail(
+                elevation: 1,
+                extended: dimens.maxWidth >= 800,
+                minExtendedWidth: 180,
+                selectedIndex: index,
+                onDestinationSelected: _selectScreen,
+                destinations: destinations
+                    .map((e) => NavigationRailDestination(
+                        icon: e.icon, label: Text(e.label)))
+                    .toList()),
+            Expanded(child: _screen() ?? Container()),
           ]),
-      body: _screen(),
-    );
+        );
+      }
+      return Scaffold(
+        bottomNavigationBar: NavigationBar(
+            selectedIndex: index,
+            onDestinationSelected: _selectScreen,
+            destinations: destinations
+                .map((e) => NavigationDestination(icon: e.icon, label: e.label))
+                .toList()),
+        body: _screen(),
+      );
+    });
   }
 
   Widget? _screen() {
